1216ff09e3602f387bc970db2fc7c68913e4417b
GovindPatil75 <mulegr75@gmail.com>
2024-03-07 09:31:34 +0530

    Batch 24

---
+++ src/test/java/com/Test/FirstATScript.java
@@ -0,0 +1,22 @@
+package com.Test;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class FirstATScript {
+
+	public static void main(String[] args) {
+		
+		//WebDriver driver=new ChromeDriver();
+		
+		ChromeDriver driver=new ChromeDriver();
+		
+		driver.get(null);
+		
+		driver.findElement(null);
+		
+		driver.getTitle();
+
+	}
+
+}

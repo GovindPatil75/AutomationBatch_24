308576237fa0933d0b5d4ccd07988d68bc9fa402
GovindPatil75 <mulegr75@gmail.com>
2024-04-15 09:16:55 +0530

    POM with PageFactory

---
+++ src/test/java/com/CrossBrowser/VerifyTest_T7.java
@@ -0,0 +1,40 @@
+package com.CrossBrowser;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.Test;
+
+import com.PageObjectRepo.LoginPom;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class VerifyTest_T7 {
+
+	
+	@Test
+	public void verifyFBTC_001() {
+		
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://www.facebook.com/");
+		driver.manage().window().maximize();
+		
+		// WebElement -- 
+		
+		LoginPom login=PageFactory.initElements(driver, LoginPom.class);
+		
+		login.getEmail().sendKeys("Test");
+		login.getPassword().sendKeys("12345678");
+		login.getLoginBtn().click();
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+}
---
+++ src/test/java/com/PageObjectRepo/LoginPom.java
@@ -0,0 +1,31 @@
+package com.PageObjectRepo;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+
+public class LoginPom {
+	
+	@FindBy(how=How.XPATH,using="//input[@id='email']") 
+	private WebElement Email;
+	
+	@FindBy(how=How.XPATH,using="//input[@id='pass']") 
+	private WebElement Password;
+	
+	@FindBy(how=How.XPATH,using="//button[@name='login']") 
+	private WebElement LoginBtn;
+
+	
+	public WebElement getEmail() {
+		return Email;
+	}
+
+	public WebElement getPassword() {
+		return Password;
+	}
+
+	public WebElement getLoginBtn() {
+		return LoginBtn;
+	}
+	
+}

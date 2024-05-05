b08c762966f71d89bb949c34ece8ea95984b4c76
GovindPatil75 <mulegr75@gmail.com>
2024-05-05 09:24:12 +0530

    Waits

---
+++ src/test/java/com/CrossBrowser/Test_1.java
@@ -0,0 +1,13 @@
+package com.CrossBrowser;
+
+import org.testng.annotations.Test;
+
+public class Test_1 {
+	
+	@Test
+	public void t1() {
+		
+		//WebElement element=driver.findElement(By.xpath("Xpath Expression"));
+	}
+
+}
---
+++ src/test/java/com/CrossBrowser/Test_2.java
@@ -0,0 +1,12 @@
+package com.CrossBrowser;
+
+import org.testng.annotations.Test;
+
+public class Test_2 {
+
+	
+	@Test
+	public void t2() {
+		//WebElement element=driver.findElement(By.xpath("Xpath Expression"));
+	}
+}

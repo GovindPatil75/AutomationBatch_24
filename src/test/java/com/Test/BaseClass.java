f4b7ab335fc284447247b2d112ba825f93f5460b
GovindPatil75 <mulegr75@gmail.com>
2024-05-05 10:15:26 +0530

    message

--- src/test/java/com/Test/BaseClass.java
+++ src/test/java/com/Test/BaseClass.java
@@ -1,10 +1,13 @@
 package com.Test;
 
+import java.time.Duration;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
+import dev.failsafe.internal.util.Durations;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class BaseClass {
@@ -19,6 +22,7 @@ public class BaseClass {
 		
 		WebDriverManager.chromedriver().setup();
 		driver=new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 		driver.get("https://www.facebook.com/");
 		driver.manage().window().maximize();
 	}
